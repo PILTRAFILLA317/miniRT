8f89631ef7ba0828c731790a077d43cb7f296863
Benat Castro <becastro@c4r6s2.42urduliz.com>
2023-03-03 15:54:33 +0100

    cyl_inter norme ok

--- srcs/cyl_inter.c
+++ srcs/cyl_inter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cyl_inter.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:44:22 by umartin-          #+#    #+#             */
-/*   Updated: 2023/02/20 18:21:37 by umartin-         ###   ########.fr       */
+/*   Updated: 2023/03/03 15:52:22 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,57 @@
 
 t_vec	cyl_intersect_point(t_vec pos, t_cyl *cyl, t_vec dir)
 {
-	double	a;
-	double	b;
-	double	c;
-	double	d;
+	double	i[4];
 	double	t1;
 	t_vec	x;
 
 	x = vec_diff(pos, cyl->pos);
-	a = vec_dot(dir, dir) - (vec_dot(dir, cyl->orient)
+	i[0] = vec_dot(dir, dir) - (vec_dot(dir, cyl->orient)
 			* vec_dot(dir, cyl->orient));
-	b = (vec_dot(dir, x) - vec_dot(dir, cyl->orient)
+	i[1] = (vec_dot(dir, x) - vec_dot(dir, cyl->orient)
 			* vec_dot(x, cyl->orient)) * 2;
-	c = vec_dot(x, x) - (vec_dot(x, cyl->orient)
+	i[2] = vec_dot(x, x) - (vec_dot(x, cyl->orient)
 			* vec_dot(x, cyl->orient)) - ((cyl->diam / 2) * (cyl->diam / 2));
-	d = b * b - 4 * a * c;
-	if (d >= 0)
+	i[3] = i[1] * i[1] - 4 * i[0] * i[2];
+	if (i[3] >= 0)
 	{
-		t1 = (-b - sqrt(d)) / (2 * a);
+		t1 = (-i[1] - sqrt(i[3])) / (2 * i[0]);
 		return (vec_point(dir, pos, t1));
 	}
 	return (cyl->pos);
 }
 
+void	cyl_init(double *i, t_vec *pos, t_cyl *cyl, t_vec *dir)
+{
+	t_vec	x;
+
+	x = vec_diff(*pos, cyl->pos);
+	i[0] = vec_dot(*dir, *dir) - (vec_dot(*dir, cyl->orient)
+			* vec_dot(*dir, cyl->orient));
+	i[1] = (vec_dot(*dir, x) - vec_dot(*dir, cyl->orient)
+			* vec_dot(x, cyl->orient)) * 2;
+	i[2] = vec_dot(x, x) - (vec_dot(x, cyl->orient)
+			* vec_dot(x, cyl->orient)) - ((cyl->diam / 2) * (cyl->diam / 2));
+	i[3] = i[1] * i[1] - 4 * i[0] * i[2];
+}
+
 int	cyl_intersect(t_vec pos, t_cyl *cyl, t_vec dir)
 {
-	double	a;
-	double	b;
-	double	c;
-	double	d;
+	double	i[4];
 	double	t1;
 	t_vec	p;
-	t_vec	x;
-	double	a1;
-	double	a2;
+	double	a[2];
 
-	x = vec_diff(pos, cyl->pos);
-	a = vec_dot(dir, dir) - (vec_dot(dir, cyl->orient)
-			* vec_dot(dir, cyl->orient));
-	b = (vec_dot(dir, x) - vec_dot(dir, cyl->orient)
-			* vec_dot(x, cyl->orient)) * 2;
-	c = vec_dot(x, x) - (vec_dot(x, cyl->orient)
-			* vec_dot(x, cyl->orient)) - ((cyl->diam / 2) * (cyl->diam / 2));
-	d = b * b - 4 * a * c;
-	if (d >= 0)
+	cyl_init(i, &pos, cyl, &dir);
+	if (i[3] >= 0)
 	{
-		t1 = (-b - sqrt(d)) / (2 * a);
+		t1 = (-i[1] - sqrt(i[3])) / (2 * i[0]);
 		if (t1 > 0)
 		{
 			p = vec_point(dir, pos, t1);
-			a1 = vec_dot(vec_norm(points_to_vec(cyl->pos, p)), cyl->orient);
-			a2 = vec_dot(vec_norm(points_to_vec(cyl->pos2, p)), cyl->orient2);
-			if (a1 >= 0 && a2 >= 0)
+			a[0] = vec_dot(vec_norm(points_to_vec(cyl->pos, p)), cyl->orient);
+			a[1] = vec_dot(vec_norm(points_to_vec(cyl->pos2, p)), cyl->orient2);
+			if (a[0] >= 0 && a[1] >= 0)
 				return (1);
 		}
 	}
