6042e91f17c6e018f04b0c14489864b232085376
Benat Castro <becastro@c4r6s2.42urduliz.com>
2023-03-06 18:30:31 +0100

    sph inter norme ok

--- srcs/sph_inter.c
+++ srcs/sph_inter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sph_inter.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:44:01 by umartin-          #+#    #+#             */
-/*   Updated: 2023/01/27 21:16:38 by umartin-         ###   ########.fr       */
+/*   Updated: 2023/03/06 18:28:47 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,22 @@ t_vec	sph_intersect_point(t_vec pos, t_sphere *sph, t_vec dir)
 {
 	t_vec	inter_point;
 	t_vec	l;
-	float	thc;
-	float	t0;
-	float	t1;
-	float	ld;
-	float	tca;
-	float	d;
+	float	th[2];
+	float	t[2];
+	float	d[2];
 
 	l.x = sph->pos.x - pos.x;
 	l.y = sph->pos.y - pos.y;
 	l.z = sph->pos.z - pos.z;
-	ld = vec_len(l);
-	tca = vec_dot(l, dir);
-	d = sqrt((ld * ld) - (tca * tca));
-	thc = sqrt(((sph->diam / 2) * (sph->diam / 2)) - (d * d));
-	t0 = tca - thc;
-	t1 = tca + thc;
-	inter_point.x = pos.x + (dir.x * t0);
-	inter_point.y = pos.y + (dir.y * t0);
-	inter_point.z = pos.z + (dir.z * t0);
+	d[0] = vec_len(l);
+	th[1] = vec_dot(l, dir);
+	d[1] = sqrt((d[0] * d[0]) - (th[1] * th[1]));
+	th[0] = sqrt(((sph->diam / 2) * (sph->diam / 2)) - (d[1] * d[1]));
+	t[0] = th[1] - th[0];
+	t[1] = th[1] + th[0];
+	inter_point.x = pos.x + (dir.x * t[0]);
+	inter_point.y = pos.y + (dir.y * t[0]);
+	inter_point.z = pos.z + (dir.z * t[0]);
 	return (inter_point);
 }
 
