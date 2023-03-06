fa01a020a50c822fef799c531e7e623be07c58de
Benat Castro <becastro@c4r6s2.42urduliz.com>
2023-03-06 18:42:28 +0100

    tri inter norme ok

--- srcs/tri_inter.c
+++ srcs/tri_inter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tri_inter.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 18:28:39 by umartin-          #+#    #+#             */
-/*   Updated: 2023/02/11 16:33:06 by umartin-         ###   ########.fr       */
+/*   Updated: 2023/03/06 18:38:27y becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,43 +24,42 @@ t_vec	t_intersect_point(t_vec pos, t_tri *tri, t_vec dir)
 	return (vec_point(dir, pos, t));
 }
 
+static int	intersect_checker(t_vec *vec, t_tri *tri)
+{
+	if (vec_dot(vec[2], tri->orient) < 0)
+		return (0);
+	vec[4] = vec_diff(tri->ver[2], tri->ver[1]);
+	vec[7] = vec_diff(vec[1], tri->ver[1]);
+	vec[2] = vec_cross(vec[4], vec[7]);
+	if (vec_dot(vec[2], tri->orient) < 0)
+		return (0);
+	return (1);
+}
+
 int	tri_intersect(t_vec pos, t_tri *tri, t_vec dir)
 {
 	double	t;
 	double	denom;
-	t_vec	p0l0;
-	t_vec	ip;
-	t_vec	c;
-	t_vec	edge0;
-	t_vec	edge1;
-	t_vec	edge2;
-	t_vec	vp0;
-	t_vec	vp1;
-	t_vec	vp2;
+	t_vec	vec[9];
 
 	denom = vec_dot(tri->orient, dir);
 	if (denom != 0)
 	{
-		p0l0 = points_to_vec(pos, tri->ver[0]);
-		t = vec_dot(p0l0, tri->orient) / denom;
+		vec[0] = points_to_vec(pos, tri->ver[0]);
+		t = vec_dot(vec[0], tri->orient) / denom;
 		if (t < 0.0)
 			return (0);
 	}
-	ip = t_intersect_point(pos, tri, dir);
-	edge0 = vec_diff(tri->ver[1], tri->ver[0]);
-	vp0 = vec_diff(ip, tri->ver[0]);
-	c = vec_cross(edge0, vp0);
-	if (vec_dot(c, tri->orient) < 0)
+	vec[1] = t_intersect_point(pos, tri, dir);
+	vec[3] = vec_diff(tri->ver[1], tri->ver[0]);
+	vec[6] = vec_diff(vec[1], tri->ver[0]);
+	vec[2] = vec_cross(vec[3], vec[6]);
+	if (!intersect_checker(vec, tri))
 		return (0);
-	edge1 = vec_diff(tri->ver[2], tri->ver[1]);
-	vp1 = vec_diff(ip, tri->ver[1]);
-	c = vec_cross(edge1, vp1);
-	if (vec_dot(c, tri->orient) < 0)
-		return (0);
-	edge2 = vec_diff(tri->ver[0], tri->ver[2]);
-	vp2 = vec_diff(ip, tri->ver[2]);
-	c = vec_cross(edge2, vp2);
-	if (vec_dot(c, tri->orient) < 0)
+	vec[5] = vec_diff(tri->ver[0], tri->ver[2]);
+	vec[8] = vec_diff(vec[1], tri->ver[2]);
+	vec[2] = vec_cross(vec[5], vec[8]);
+	if (vec_dot(vec[2], tri->orient) < 0)
 		return (0);
 	return (1);
 }
