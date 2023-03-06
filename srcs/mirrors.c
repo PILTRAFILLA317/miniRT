444dea146c9e3f1a08d1785b2d973ff0899b2584
Benat Castro <becastro@c4r6s2.42urduliz.com>
2023-03-06 15:23:13 +0100

    pl_mirror && sph_mirror norme ok

--- srcs/mirrors.c
+++ srcs/mirrors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mirrors.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 19:32:54 by umartin-          #+#    #+#             */
-/*   Updated: 2023/03/01 19:22:40 by umartin-         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:21:25 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,58 @@
 
 int	pl_mirror(t_elem *elem, t_dirpos rt, t_plane pl, int *ray)
 {
-	t_vec		ref;
-	t_vec		alight;
-	t_vec		light;
-	t_vec		final;
-	t_vec		rgb;
+	t_vec		vec[5];
 	t_dirpos	rf;
 	t_object	obj;
 
 	obj.type = 0;
 	obj.elem = &pl;
-	ref = vec_mult_vec(pl.orient, rt.dir);
-	ref = vec_mult_vec(ref, pl.orient);
-	ref = vec_mult (ref, -2);
-	ref = vec_add(rt.dir, ref);
-	ref = vec_norm(ref);
-	alight = (vec_mult(vec_mult_vec(col_to_01(pl.color),
+	vec[0] = vec_mult_vec(pl.orient, rt.dir);
+	vec[0] = vec_mult_vec(vec[0], pl.orient);
+	vec[0] = vec_mult (vec[0], -2);
+	vec[0] = vec_add(rt.dir, vec[0]);
+	vec[0] = vec_norm(vec[0]);
+	vec[1] = (vec_mult(vec_mult_vec(col_to_01(pl.color),
 					col_to_01(elem->alight.color)), elem->alight.ratio));
-	light = light_comb_pl(pl, elem, rt.pos);
-	final = vec_add(alight, light);
-	final = vec_clamp(0, 1, final);
-	final = vec_mult(final, (1 - pl.ref));
-	rf.dir = ref;
+	vec[2] = light_comb_pl(pl, elem, rt.pos);
+	vec[3] = vec_add(vec[1], vec[2]);
+	vec[3] = vec_clamp(0, 1, vec[3]);
+	vec[3] = vec_mult(vec[3], (1 - pl.ref));
+	rf.dir = vec[0];
 	rf.pos = rt.pos;
 	(*ray)++;
-	rgb = col_to_01(double_to_rgb((color(elem, rf, obj, ray))));
-	rgb = vec_mult(rgb, pl.ref);
-	final = vec_add(final, rgb);
-	return (convert_rgb(col_to_255(final)));
+	vec[4] = col_to_01(double_to_rgb((color(elem, rf, obj, ray))));
+	vec[4] = vec_mult(vec[4], pl.ref);
+	vec[3] = vec_add(vec[3], vec[4]);
+	return (convert_rgb(col_to_255(vec[3])));
 }
 
 int	sph_mirror(t_elem *elem, t_dirpos rt, t_sphere sph, int *ray)
 {
-	t_vec		ref;
-	t_vec		alight;
-	t_vec		light;
-	t_vec		final;
-	t_vec		rgb;
+	t_vec		vec[5];
 	t_dirpos	rf;
 	t_object	obj;
 
 	obj.type = 1;
 	obj.elem = &sph;
-	ref = vec_mult(vec_diff(rt.pos, sph.pos), vec_dot(rt.dir, vec_diff(rt.pos, sph.pos)));
-	ref = vec_mult (ref, -2);
-	ref = vec_add(rt.pos, ref);
-	ref = vec_norm(ref);
-	alight = (vec_mult(vec_mult_vec(col_to_01(sph.color),
+	vec[0] = vec_mult(vec_diff(rt.pos, sph.pos),
+			vec_dot(rt.dir, vec_diff(rt.pos, sph.pos)));
+	vec[0] = vec_mult (vec[0], -2);
+	vec[0] = vec_add(rt.pos, vec[0]);
+	vec[0] = vec_norm(vec[0]);
+	vec[1] = (vec_mult(vec_mult_vec(col_to_01(sph.color),
 					col_to_01(elem->alight.color)), elem->alight.ratio));
-	light = light_comb_sph(sph, elem, rt.pos);
-	final = vec_add(alight, light);
-	final = vec_clamp(0, 1, final);
-	final = vec_mult(final, (1 - sph.ref));
-	rf.dir = ref;
+	vec[2] = light_comb_sph(sph, elem, rt.pos);
+	vec[3] = vec_add(vec[1], vec[2]);
+	vec[3] = vec_clamp(0, 1, vec[3]);
+	vec[3] = vec_mult(vec[3], (1 - sph.ref));
+	rf.dir = vec[0];
 	rf.pos = rt.pos;
 	(*ray)++;
-	rgb = col_to_01(double_to_rgb((color(elem, rf, obj, ray))));
-	rgb = vec_mult(rgb, sph.ref);
-	final = vec_add(final, rgb);
-	return (convert_rgb(col_to_255(final)));
+	vec[4] = col_to_01(double_to_rgb((color(elem, rf, obj, ray))));
+	vec[4] = vec_mult(vec[4], sph.ref);
+	vec[3] = vec_add(vec[3], vec[4]);
+	return (convert_rgb(col_to_255(vec[3])));
 }
 
 int	pl_checkboard(t_elem *elem, t_dirpos rt, t_plane pl, int *ray)
